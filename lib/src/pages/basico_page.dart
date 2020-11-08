6d3bcaa9ba469a16a0197e023c1eac4420d990b0
Freddy <freddy.artea@gmail.com>
2020-11-08 00:37:30 -0500

    alineacion Column y Row

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
 
 class BasicoPage extends StatelessWidget {
-  const BasicoPage({Key key}) : super(key: key);
-
+  final estiloTitulo = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+  final estiloSubtitulo = TextStyle(fontSize: 18.0, color: Colors.grey);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Basico Page')));
+    return Scaffold(
+        body: Column(
+      children: <Widget>[
+        Image(
+            image: NetworkImage(
+                'https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Lago con un Puente',
+                      style: estiloTitulo,
+                    ),
+                    SizedBox(
+                      height: 7.0,
+                    ),
+                    Text('un lago en Alemania', style: estiloSubtitulo),
+                  ],
+                ),
+              ),
+              Icon(Icons.star, color: Colors.red, size: 30.0),
+              Text('41', style: TextStyle(fontSize: 20.0)),
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
