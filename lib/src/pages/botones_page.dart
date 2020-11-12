9111b12bcab991a4c3b004a11f58a1fc5c8c2d6f
Freddy <freddy.artea@gmail.com>
2020-11-12 12:24:52 -0500

    Bottom Navigation Bar

--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -6,8 +6,36 @@ class BotonesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [_fondoApp()],
+        body: Stack(
+          children: <Widget>[
+            _fondoApp(),
+            SingleChildScrollView(
+              child: Column(
+                children: <Widget>[_titulos()],
+              ),
+            )
+          ],
+        ),
+        bottomNavigationBar: _bottomNavigationBar(context));
+  }
+
+  Widget _bottomNavigationBar(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+          canvasColor: Color.fromRGBO(55, 57, 84, 1.0),
+          primaryColor: Colors.pinkAccent,
+          textTheme: Theme.of(context).textTheme.copyWith(
+              caption: TextStyle(color: Color.fromRGBO(116, 117, 152, 1.0)))),
+      child: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today, size: 30.0), title: Container()),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.bubble_chart, size: 30.0), title: Container()),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.supervised_user_circle, size: 30.0),
+              title: Container()),
+        ],
       ),
     );
   }
@@ -47,6 +75,36 @@ class BotonesPage extends StatelessWidget {
           child: cajaRosa,
         )
       ],
+    );
+  }
+
+  Widget _titulos() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Classify Transaction',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Classify this transaction in to a paraticular categeoery',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
